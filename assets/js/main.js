5b86f19d4e00ea5a6c33b12f4de839736c339c55
Ahmed Elgendy <develgendy@gmail.com>
2021-02-19 03:05:57 +0200

    add new edit and add page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1181,6 +1181,36 @@ $(function() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
     // ------------step-wizard-------------
 $(document).ready(function () {
     $('.nav-tabs > li a[title]').tooltip();
@@ -1223,6 +1253,37 @@ function prevTab(elem) {
     
     /*=====  End of   ======*/
     
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
     
     
@@ -1315,3 +1376,65 @@ function prevTab(elem) {
 
 
 
+$(function() {
+
+    var owner = $('#owner');
+    var cardNumber = $('#cardNumber');
+    var cardNumberField = $('#card-number-field');
+    var CVV = $("#cvv");
+    var mastercard = $("#mastercard");
+    var confirmButton = $('#confirm-purchase');
+    var visa = $("#visa");
+    var amex = $("#amex");
+
+    // Use the payform library to format and validate
+    // the payment fields.
+
+    cardNumber.payform('formatCardNumber');
+    CVV.payform('formatCardCVC');
+
+
+    cardNumber.keyup(function() {
+
+        amex.removeClass('transparent');
+        visa.removeClass('transparent');
+        mastercard.removeClass('transparent');
+
+        if ($.payform.validateCardNumber(cardNumber.val()) == false) {
+            cardNumberField.addClass('has-error');
+        } else {
+            cardNumberField.removeClass('has-error');
+            cardNumberField.addClass('has-success');
+        }
+
+        if ($.payform.parseCardType(cardNumber.val()) == 'visa') {
+            mastercard.addClass('transparent');
+            amex.addClass('transparent');
+        } else if ($.payform.parseCardType(cardNumber.val()) == 'amex') {
+            mastercard.addClass('transparent');
+            visa.addClass('transparent');
+        } else if ($.payform.parseCardType(cardNumber.val()) == 'mastercard') {
+            amex.addClass('transparent');
+            visa.addClass('transparent');
+        }
+    });
+
+    confirmButton.click(function(e) {
+
+        e.preventDefault();
+
+        var isCardValid = $.payform.validateCardNumber(cardNumber.val());
+        var isCvvValid = $.payform.validateCardCVC(CVV.val());
+
+        if(owner.val().length < 5){
+            alert("Wrong owner name");
+        } else if (!isCardValid) {
+            alert("Wrong card number");
+        } else if (!isCvvValid) {
+            alert("Wrong CVV");
+        } else {
+            // Everything is correct. Add your form submission code here.
+            alert("Everything is correct");
+        }
+    });
+});
